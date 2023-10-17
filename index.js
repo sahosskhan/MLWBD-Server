790b3c3201bb9e330fc786300f7c292cc84ebe95
MuhammadSahossKhan <sahosskhan359@gmail.com>
2023-10-18 04:04:54 +0600

    Basic Setup Update & Server Run Test

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome To My Server");
+});
+
+app.listen(port, () => {
+  console.log(`listening on port , ${port}`);
+});
